9dd6cf4a4be3ec0ecf4a7538447cf50c5ddb96f8
aditi <additigupta366@gmail.com>
2025-02-10 22:06:38 +0530

    if else and loops done

---
+++ basic/ifElse.js
@@ -0,0 +1,34 @@
+// ********** if else condition and ternary operator ****************
+let age = 18;
+let fname = "adi";
+// if (age >= 18) {
+//   console.log("user can play game");
+// } else {
+//   console.log(" cannot");
+// }
+
+// //ternary operator
+// let opt = age >= 18 ? "coffee" : "choco";
+// console.log(opt);
+
+//&& and ||
+// if (fname[0] === "a" && age >= 18) {
+//   console.log("if case executes ");
+// } else {
+//   console.log("else executes");
+// }
+
+//******nested if else  -if , else if, else*/
+
+/* ============ while =============== */
+let i = 0;
+let sum = 0;
+while (i <= 3) {
+  console.log(i);
+  sum = sum + i;
+  ++i;
+}
+console.log(`current i is ${i}`);
+console.log(sum);
+
+//++++++++++++++++ for looop => same as c++ ================
