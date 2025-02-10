33168c352cbb3d3f34d6f1852054ca29638b062b
aditi <additigupta366@gmail.com>
2025-02-10 16:44:14 +0530

    basic comparison done

--- advance/new.js
+++ advance/new.js
@@ -1,1 +1,30 @@
-console.log("advance content ");
+// console.log("advance content ");
+let gameCounter = 100;
+++gameCounter;
+// console.log(gameCounter);
+
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 < 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null != 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined != 0);
+
+// console.log("2" === 1);  // data tyoe is not same
+
+let nam = "aditigupta";
+// console.log(nam[nam.length - 2]);
+for (let i = 1; i < nam.length; i++) {
+  console.log(nam[i]);
+}
